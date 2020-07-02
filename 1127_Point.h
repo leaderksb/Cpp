49a8b13a8c65348f78569586ad90793cf4138bc4
토끼깡총총총 <55656673+remnantleader@users.noreply.github.com>
2020-07-02 18:09:02 +0900

    Create 1127_Point.h

---
+++ 1127_Point.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#ifndef POINT_H
+#define POINT_H
+
+class Point {
+	int x, y;
+public:
+	void set(int x, int y) { this->x = x; this->y = y; }
+	void showPoint() {
+		cout << "(" << x << ", " << y << ")" << endl;
+	}
+};
+
+#endif
